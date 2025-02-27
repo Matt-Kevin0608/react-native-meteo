6872cd147918f663341474fdf80345ea365da097
Matt-Kevin0608 <caoyangxiaoyao@gmail.com>
2025-02-27 13:05:08 +0800

    init project and done layout

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { s } from "./App.style";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Home } from "./pages/home/Home";
+import { ImageBackground } from "react-native";
+import backgroundImg from "./assets/background.png";
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lon: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({
+        lat: "48.85",
+        lon: "2.35",
+      });
+    }
+  }
+
+  console.log(coordinates);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={backgroundImg}
+      style={s.img_background}
+      imageStyle={s.img}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView style={s.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
